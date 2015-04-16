46e3745bf59e3da1d5af73c959310dc03760877b
Stéphane Manciot <stephane.manciot@ebiznext.com>
2015-04-16 07:34:06 +0200

    check file formats

--- src/test/scala/com/mogobiz/utils/MimeTypeToolsSpec.scala
+++ src/test/scala/com/mogobiz/utils/MimeTypeToolsSpec.scala
@@ -16,18 +16,24 @@ class MimeTypeToolsSpec extends Specification{
       file exists() must_== true
       val mimeType = MimeTypeTools.detectMimeType(file).getOrElse("unknown")
       mimeType must_== "application/pdf"
+      val format = MimeTypeTools.toFormat(file).getOrElse("unknown")
+      format must_== "pdf"
     }
     "detect image/jpeg mime type" in {
       val file = new File(MimeTypeTools.getClass.getResource("image1").getPath)
       file exists() must_== true
       val mimeType = MimeTypeTools.detectMimeType(file).getOrElse("unknown")
       mimeType must_== "image/jpeg"
+      val format = MimeTypeTools.toFormat(file).getOrElse("unknown")
+      format must_== "jpeg"
     }
     "detect image/png mime type" in {
       val file = new File(MimeTypeTools.getClass.getResource("image2").getPath)
       file exists() must_== true
       val mimeType = MimeTypeTools.detectMimeType(file).getOrElse("unknown")
       mimeType must_== "image/png"
+      val format = MimeTypeTools.toFormat(file).getOrElse("unknown")
+      format must_== "png"
     }
   }
 
