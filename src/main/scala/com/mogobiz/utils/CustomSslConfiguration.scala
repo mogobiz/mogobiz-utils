4f30c672d4649fc33e765d03addc9ad1eb3c3d86
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2016-06-14 07:37:42 +0200

    Initial migration

--- src/main/scala/com/mogobiz/utils/CustomSslConfiguration.scala
+++ src/main/scala/com/mogobiz/utils/CustomSslConfiguration.scala
@@ -7,18 +7,14 @@ package com.mogobiz.utils
 import java.security.cert.X509Certificate
 import javax.net.ssl.{KeyManager, SSLContext, X509TrustManager}
 
+import akka.http.javadsl.HttpsConnectionContext
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
-import akka.http.scaladsl.model.Uri.Host
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.io.IO
-import akka.pattern.ask
+import akka.http.scaladsl.model._
 import akka.stream.scaladsl.{Sink, Source}
 import akka.util.Timeout
-import com.mogobiz.system.ActorSystemLocator
 import com.typesafe.sslconfig.akka.AkkaSSLConfig
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 
 trait CustomSslConfiguration {
   implicit val trustfulSslContext: SSLContext = {
@@ -34,27 +30,32 @@ trait CustomSslConfiguration {
     context.init(Array[KeyManager](), Array(BlindFaithX509TrustManager), null)
     context
   }
+  val trustfulClientContext: HttpsConnectionContext = new HttpsConnectionContext(trustfulSslContext)
 
   implicit def timeout: Timeout
 
-  val badSslConfig = AkkaSSLConfig().mapSettings(s => s.withLoose(s.loose.withDisableSNI(true)))
+  val badSslConfig = AkkaSSLConfig().mapSettings(s => s.withLoose(s.loose.withAcceptAnyCertificate(true).
+    withDisableHostnameVerification(true).
+    withAllowLegacyHelloMessages(Some(true)).
+    withAllowUnsafeRenegotiation(Some(true)).
+    withAllowWeakCiphers(true).
+    withAllowWeakProtocols(true).
+    withDisableSNI(true)))
+
   val badCtx = Http().createClientHttpsContext(badSslConfig)
-  val connectionFlow = Http().outgoingConnectionHttps(unsafeHost, connectionContext = badCtx)
+  val connectionFlow = Http().outgoingConnectionHttps(host = "", port = 443, connectionContext = badCtx)
+
+  val request = HttpRequest(
+    method = HttpMethods.POST,
+    uri = Uri("/test"),
+    entity = HttpEntity(MediaTypes.`text/plain`, "")
+  )
 
   def httpsRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request).via(connectionFlow).runWith(Sink.head)
 
-    response.status match {
-      case StatusCodes.OK => System.err.println(s"The mapping for `$name` was successfully set.")
-
-      case _ =>
-        // System.err.println(s"Error while setting the mapping for `$name`: ${response.entity.toStrict(5 seconds).map(_.data.toString())}")
-        Unmarshal(response.entity).to[String].map { data =>
-          System.err.println(s"Error while setting the mapping for `$name`: ${data}")
-        }
-    }
-  }
   Await.result(singleResult, 10 seconds)
 
+}
   /*
         val request = HttpRequest(
         method = HttpMethods.POST,
@@ -85,4 +86,42 @@ trait CustomSslConfiguration {
     Http().outgoingConnectionHttps(unsafeHost, connectionContext = badCtx)
 
    */
+
+/*
+trait CustomSslConfiguration {
+  implicit val trustfulSslContext: SSLContext = {
+    object BlindFaithX509TrustManager extends X509TrustManager {
+      def checkClientTrusted(chain: Array[X509Certificate], authType: String) = ()
+
+      def checkServerTrusted(chain: Array[X509Certificate], authType: String) = ()
+
+      def getAcceptedIssuers = Array[X509Certificate]()
+    }
+
+    val context = SSLContext.getInstance("TLS")
+    context.init(Array[KeyManager](), Array(BlindFaithX509TrustManager), null)
+    context
+  }
+
+  implicit val sslEngineProvider: ClientSSLEngineProvider = {
+    // To enable TLS 1.2 you can use the following setting of the VM: -Dhttps.protocols=TLSv1.1,TLSv1.2
+    ClientSSLEngineProvider { engine =>
+      engine.setEnabledProtocols(Array("TLSv1.2", "TLSv1.1", "SSLv3"))
+      engine
+    }
+  }
+  implicit def timeout: Timeout
+
+  def sslPipeline(host: Host): Future[SendReceive] = {
+    implicit val system = ActorSystemLocator()
+    implicit val _ = system.dispatcher
+    val logRequest: HttpRequest => HttpRequest = { r => println(r); r }
+    val logResponse: HttpResponse => HttpResponse = { r => println(r); r }
+    for (
+      Http.HostConnectorInfo(connector, _) <- IO(Http) ? Http.HostConnectorSetup(host.toString, 443, sslEncryption = true)(system, sslEngineProvider)
+    ) yield logRequest ~> sendReceive(connector) ~> logResponse
+
+  }
 }
+
+ */
